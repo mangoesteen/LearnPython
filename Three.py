a18ba27f29adebd6ff5f8ec9c1594b264ecc8a05
nsawaengsri <fruity43@hotmail.com>
2019-06-17 08:51:13 +0700

    try except

--- Three.py
+++ Three.py
@@ -18,3 +18,25 @@ a.deposit(100)
 b.deposit(50)
 b.withdraw(10)
 a.withdraw(10)
+
+
+# handling_exception.py #error
+try:
+    print(1/0)
+except ZeroDivisionError:
+    print("You can't divide by zero")
+
+#raising_error.py
+try:
+    a = int(input("Enter a negative integer: "))
+    if a >= 0:
+        raise ValueError("That is not a negative number!")
+except ValueError as ve:
+    print(ve)
+
+#try…finally
+try:
+    f = open("test.txt", encoding='utf-8')
+    # perform file operations
+finally:
+    f.close()
